7f75970377db3f9cd1e5819a85cca02a7b637bad
0ve4ka <turbosvin90@mail.ru>
2024-09-18 14:29:14 +0300

    Сдаю первое задание за 18 число

--- Binary/src/Main.java
+++ Binary/src/Main.java
@@ -1,39 +1,21 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Integer> speeds = new ArrayList<>(); // объявите список
+        speeds.add(120);
+        speeds.add(75);
+        speeds.add(42);
+        speeds.add(60);
+        speeds.add(110);
+        speeds.add(20);
 
-
-        String pixelKcalDay = "43"; // столько калорий съел Пиксель до похода к бабушке
-        String beefKcal = "30.2"; // калорийность говядины
-        String chickenKcal = "23.8"; // калорийность курицы
-        String creamKcal = "32.1"; // калорийность сливок
-        String milkKcal = "13.5"; // калорийность молока
-
-        float pixelChoice = getMinKcalsSum(beefKcal, chickenKcal, creamKcal, milkKcal);
-        float totalKcal = Integer.parseInt(pixelKcalDay) + pixelChoice;
-        checkKcal(totalKcal);
-    }
-
-    private static Float getMinKcalsSum(String firstDishKcal, String secondDishKcal, String firstDesert, String secondDesert) {
-        float minDishKcal = Float.min(Float.parseFloat(firstDishKcal),Float.parseFloat(secondDishKcal)); // вычислите минимальную калорийность основного блюда
-        float minDesertKcal = Float.min(Float.parseFloat(firstDesert),Float.parseFloat(secondDesert)); // вычислите минимальную калорийность десерта
-        return minDishKcal + minDesertKcal;
-    }
-
-    private static void checkKcal(Float catKcal) {
-        if (catKcal == null) {
-            System.out.println("Что-то пошло не так");
-        }
-        else {
-            System.out.println("Калорийность рациона Пикселя за день: " + catKcal);
-            if (catKcal > 100) {
-                System.out.println("Пиксель сегодня не уложился в норму.");
-            } else {
-                System.out.println("Лимит не превышен!");
-            }
+        int sum = 0;
+        for (int speed : speeds) {
+            sum += speed;  // сложите все значения в списке
         }
 
-
-
+        int averageSpeed = sum/speeds.size(); // вычислите среднюю скорость
+        System.out.println("Средняя скорость равна " + averageSpeed + " км/ч");
     }
 }
